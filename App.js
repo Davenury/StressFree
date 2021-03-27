6bf62f01a8fd1b99bc930315245439cb5e0a595c
Dawid Dębowski <dawid199960@gmail.com>
2021-03-27 15:17:51 +0100

    Bottom Nav

--- App.js
+++ App.js
@@ -1,12 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 
 export default function App() {
+
+  const [toggled, setToggled] = useState(false)
+  const [value, setValue] = useState(0)
+
+  const toggleButton = () => {
+    setToggled(!toggled)
+  }
+
+  const getTextForButton = () => {
+    if(toggled)
+      return "Push me!"
+    return "Ouch!"
+  }
+
+  const getFragment = () => {
+    switch(value){
+      case 0:
+        <Relax />
+        break;
+      case 1:
+        <Categories />
+        break;
+      case 2:
+        <Settings />
+        break;
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title={getTextForButton()}
+        onPress={toggleButton}
+      />
       <StatusBar style="auto" />
+      <BottomNavigation
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        showLabels
+        style={{width: "100%", backgroundColor: "#fafafa", fontSize: "16px"}}>
+        <BottomNavigationAction label="Relax!" />
+        <BottomNavigationAction label="Cetegories" />
+        <BottomNavigationAction label="Settings" />
+      </BottomNavigation>
     </View>
   );
 }
@@ -17,5 +61,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    height: "100vh"
+  }
 });
