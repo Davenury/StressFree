77d6898b919f729b4de1323e21619246d761dc02
Rados13 <radoslawszuma@gmail.com>
2021-03-28 06:28:43 +0200

    Check if null SoundPlayer

--- components/relax/SoundPlayer.js
+++ components/relax/SoundPlayer.js
@@ -9,21 +9,19 @@ export function SoundPlayerComponent(props){
     const [sound, setSound] = useState();
 
     const playSound = async () => {
-        const { sound } = await Audio.Sound.createAsync(props.url);
-        setSound(sound);
-        await sound.playAsync();
+        if(props.url!==null){
+            const { sound } = await Audio.Sound.createAsync(props.url);
+            setSound(sound);
+            await sound.playAsync();
+        }
       }
-
-    const stopMusic = async () => {
-        sound.unloadAsync();
-    }
 
     useEffect(() => {
         return sound
         ? () => {
             console.log('Unloading Sound');
             sound.unloadAsync(); }
-        : undefined;
+        : null;
     }, [sound]);
 
 
@@ -34,12 +32,6 @@ export function SoundPlayerComponent(props){
     return(
         <View>
             <DotLoader size={150} color={colors.green} />
-            <Button 
-                variant="contained"
-                color="primary"
-                onClick={stopMusic}>
-                    Stop music
-            </Button>
         </View>
     )
 }
